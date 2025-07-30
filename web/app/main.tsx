0a945bebebd99d3c1aad87b1ed65b69418cb3663
Lucian Mocan <62904583+lucianmocan@users.noreply.github.com>
2025-07-29 21:02:51 -0400

    web: redirect unknown routes to root

--- web/app/main.tsx
+++ web/app/main.tsx
@@ -1,9 +1,19 @@
 /** @jsxImportSource solid-js */
 import { render } from 'solid-js/web';
 import './main.css';
-import { HashRouter, Route } from "@solidjs/router";
+import { HashRouter, Route, useNavigate } from "@solidjs/router";
+import { onMount } from 'solid-js';
 import App from './App';
 import Tutorial from "./components/tutorial/Tutorial";
+
+// This component will handle the redirection for any unmatched route.
+const NotFound = () => {
+  const navigate = useNavigate();
+  onMount(() => {
+    navigate('/', { replace: true });
+  });
+  return null; // It renders nothing, just performs the redirect.
+};
 
 const app = document.getElementById('app');
 
@@ -14,5 +24,7 @@ if (app) {
       <Route path="/tutorials/:stepName" component={Tutorial} />
       {/* Main app route */}
       <Route path="/" component={App} />
+      {/* Wildcard route must be last. It catches any path that wasn't matched above. */}
+      <Route path="*all" component={NotFound} />
     </HashRouter>, app);
 }
