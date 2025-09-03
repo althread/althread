a56a0d5f2423d89e84431f4a5ee4219904350d03
Lucian Mocan <62904583+lucianmocan@users.noreply.github.com>
2025-09-03 16:57:45 +0200

    feat(ltl): ltl binary_operator

--- interpreter/src/ast/token/binary_operator.rs
+++ interpreter/src/ast/token/binary_operator.rs
@@ -65,3 +65,46 @@ impl fmt::Display for BinaryOperator {
         write!(f, "{}", op)
     }
 }
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub enum LtlBinaryOperator {
+    Until,
+    WeakUntil,
+    Release,
+    And,
+    Or,
+    Implies,
+    Equivalent
+}
+
+impl NodeBuilder for LtlBinaryOperator {
+    fn build(mut pairs: Pairs<Rule>, filepath: &str) -> AlthreadResult<Self> {
+        let pair = pairs.next().unwrap();
+        match pair.as_rule() {
+            Rule::LTL_UNTIL_OP => Ok(Self::Until),
+            Rule::LTL_WEAK_UNTIL_OP => Ok(Self::WeakUntil),
+            Rule::LTL_RELEASE_OP => Ok(Self::Release),
+            Rule::LTL_IMPLIES_OP => Ok(Self::Implies),
+            Rule::LTL_EQUIVALENT_OP => Ok(Self::Equivalent),
+            Rule::AND_OP => Ok(Self::And),
+            Rule::OR_OP => Ok(Self::Or),
+            _ => Err(no_rule!(pair, "LtlBinaryOperator", filepath)),
+        }
+    }
+}
+
+impl fmt::Display for LtlBinaryOperator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let op = match self {
+            LtlBinaryOperator::Until => "U",
+            LtlBinaryOperator::WeakUntil => "W",
+            LtlBinaryOperator::Release => "V",
+            LtlBinaryOperator::Implies => "=>",
+            LtlBinaryOperator::Equivalent => "<=>",
+            LtlBinaryOperator::And => "&&",
+            LtlBinaryOperator::Or => "||",
+        };
+
+        write!(f, "{}", op)
+    }
+}
