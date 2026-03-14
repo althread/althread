9c25dbbe5f025efd555d060f2dbc5faf39a33272
Malo Galichet <kraquotte10@gmail.com>
2026-03-14 23:51:48 +0100

    Rajout de len sur les tuples

--- interpreter/src/compiler/stdlib.rs
+++ interpreter/src/compiler/stdlib.rs
@@ -218,6 +218,89 @@ impl Stdlib {
                     }),
                 });
             }
+            DataType::Tuple(t) => {
+                new_interfaces.push(Interface {
+                    name: "len".to_string(),
+                    args: vec![],
+                    ret: DataType::Integer,
+                    f: Rc::new(|list, _v, pos| {
+                        match list {
+                            Literal::Tuple(v) => Ok(Literal::Int(v.len() as i64)),
+                            _ => Err(AlthreadError::new(
+                                ErrorType::RuntimeError,
+                                pos,
+                                "Expected tuple".to_string(),
+                            )),
+                        }
+                    }),
+                });
+                // new_interfaces.push(Interface {
+                //     name: "set".to_string(),
+                //     args: vec![DataType::Integer, t.as_ref().clone()],
+                //     ret: DataType::Void,
+                //     f: Rc::new(|list, v, pos| {
+                //         let v = v.to_tuple().unwrap();
+                //         if v.len() != 2 {
+                //             return Err(AlthreadError::new(
+                //                 ErrorType::RuntimeError,
+                //                 pos,
+                //                 ".set() expects two arguments: l.set(index, value);".to_string()
+                //             ));
+                //         }
+                //         let idx = v[0].to_integer().unwrap() as usize;
+                //         if let Literal::List(dtype, list) = list {
+                //             if idx >= list.len() {
+                //                 return Err(AlthreadError::new(
+                //                     ErrorType::RuntimeError,
+                //                     pos,
+                //                     format!("Index out of bounds: {}", idx)
+                //                 ));
+                //             }
+                //             if dtype != &v[1].get_datatype() {
+                //                 return Err(AlthreadError::new(
+                //                     ErrorType::RuntimeError,
+                //                     pos,
+                //                     format!("List of type {:?} can only accept values of the same type ({} given)", dtype, v[1].get_datatype())
+                //                 ));
+                //             }
+                //             list[idx] = v[1].clone();
+                //         }
+                //         else {
+                //             return Err(AlthreadError::new(
+                //                 ErrorType::RuntimeError,
+                //                 pos,
+                //                 "Expected List".to_string()
+                //             ));
+                //         }
+                //         Ok(Literal::Null)
+                //     }),
+                // });
+                // new_interfaces.push(Interface {
+                //     name: "at".to_string(),
+                //     args: vec![DataType::Integer],
+                //     ret: t.as_ref().clone(),
+                //     f: Rc::new(|list, v, pos| {
+                //         let v = v.to_tuple().unwrap();
+                //         let v = v.first().unwrap().to_integer().unwrap();
+                //         if let Literal::List(_dtype, list) = list {
+                //             if v < 0 || v as usize >= list.len() {
+                //                 return Err(AlthreadError::new(
+                //                     ErrorType::RuntimeError,
+                //                     pos,
+                //                     format!("Index out of bounds: {}", v),
+                //                 ));
+                //             }
+                //             Ok(list[v as usize].clone())
+                //         } else {
+                //             return Err(AlthreadError::new(
+                //                 ErrorType::RuntimeError,
+                //                 pos,
+                //                 "Expected List".to_string(),
+                //             ));
+                //         }
+                //     }),
+                // });
+            }
             _ => {}
         }
 
